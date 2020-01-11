258339929e2fea3aaa2b1f11c9c6b0945193ce2e
Lars Michaelis <mail@larmic.de>
2020-01-11 13:18:28 +0100

    print out all streets first letter and corresponding url

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bremen_trash/net/http"
 	xml2 "bremen_trash/xml"
+	"container/list"
 	"encoding/xml"
 	"fmt"
 	"io"
@@ -15,14 +16,19 @@ var (
 )
 
 type Td struct {
-	A       A      `xml:"a"`
+	A       *A     `xml:"a"`
 	Class   string `xml:"class,attr"`
 	OnClick string `xml:"onClick,attr"`
 }
 
 type A struct {
-	Href string `xml:"href,attr"`
-	A    string `xml:",innerxml"`
+	Href  string `xml:"href,attr"`
+	Value string `xml:",innerxml"`
+}
+
+type FirstLetter struct {
+	FirstLetter string
+	Url         string
 }
 
 func main() {
@@ -33,7 +39,6 @@ func main() {
 	}
 
 	content = xml2.RepairInvalidHtml(content)
-	fmt.Println(content)
 
 	content = strings.ReplaceAll(content, "<br>", "")
 	content = strings.ReplaceAll(content, "</br>", "")
@@ -41,6 +46,8 @@ func main() {
 	decoder := xml.NewDecoder(strings.NewReader(content))
 	decoder.Strict = false
 	decoder.AutoClose = xml.HTMLAutoClose
+
+	firstLetters := list.New()
 
 	for {
 		token, tokenErr := decoder.Token()
@@ -58,14 +65,24 @@ func main() {
 			if matchesTd(startElement) {
 				var td Td
 				err = decoder.DecodeElement(&td, &startElement)
-				fmt.Println(err)
-				fmt.Println("a", td.A)
-				fmt.Println("Start: ", startElement.Name.Local, startElement.Attr)
+
+				if err != nil {
+					fmt.Printf("Unable to decode tag %s, Tag skipped", startElement.Name.Local)
+					continue
+				}
+
+				if td.A != nil && td.A.Href != "" && td.A.Value != "" {
+					firstLetters.PushBack(FirstLetter{td.A.Value, td.A.Href})
+				}
 			}
 		case xml.EndElement:
 			continue
 			//fmt.Println("End: ", t.Name)
 		}
+	}
+
+	for e := firstLetters.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
 	}
 }
 
